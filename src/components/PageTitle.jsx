0e7c156eb438cb7e67ef8aa7a452919acb5a4c4e
DeepakRajendran <165307612+deepakrajendran90@users.noreply.github.com>
2024-03-29 11:34:20 +0530

    Add files via upload

---
+++ src/components/PageTitle.jsx
@@ -0,0 +1,11 @@
+import { Helmet } from "react-helmet-async";
+
+const PageTitle = ({ title }) => {
+  return (
+      <title>
+        {title} | Yum Reactjs - Multipurpose Food Tailwind CSS Template
+      </title>
+  );
+};
+
+export default PageTitle;

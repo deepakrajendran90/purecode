62931698f04c32b6c347162a452932f11919a616
DeepakRajendran <165307612+deepakrajendran90@users.noreply.github.com>
2024-03-29 10:57:34 +0530

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,28 @@
+// module.exports={
+//   plugins: {
+//     "postcss-import": {},
+//     "tailwindcss/nesting": {},
+//     "tailwindcss": {},
+//     "autoprefixer": {},
+//     // require('tailwindcss')
+//   }
+// }
+
+module.exports = {
+  plugins: [
+    // require('postcss-import'),
+    // require('tailwindcss/nesting'),
+    require('tailwindcss'),
+    // require('autoprefixer'),
+  ]
+}
+
+// export default {
+//   plugins: [
+//     // "postcss-import":{},
+//     // "tailwindcss/neesting":{},
+//     // tailwindcss: {},
+//     // autoprefixer: {},
+//     require('tailwindcss'),
+//   ],
+// }
